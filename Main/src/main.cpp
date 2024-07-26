8a001146f5e90951ab72dd1136be6020486bac78
RoddurPlayz <rupaksaha530@gmail.com>
2024-07-26 23:08:18 +0600

    Removed comments in root/Main/src/main.cpp

--- Main/src/main.cpp
+++ Main/src/main.cpp
@@ -17,11 +17,9 @@ int main()
 {
     GLFWwindow *window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
     if (!window)
     {
@@ -29,7 +27,6 @@ int main()
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK)
     {
@@ -57,18 +54,14 @@ int main()
     ShaderProgram shader{ RESOURCE_PATH "vert.glsl", RESOURCE_PATH "frag.glsl" };
     shader.bind();
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
