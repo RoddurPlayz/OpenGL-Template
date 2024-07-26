9d0d2b51a418c497d7ac2d92f72330ad5245b0ff
RoddurPlayz <rupaksaha530@gmail.com>
2024-07-27 00:09:47 +0600

    Improved root/Main/src/main.cpp

--- Main/src/main.cpp
+++ Main/src/main.cpp
@@ -38,18 +38,28 @@ int main()
     title << "OpenGL: " << glGetString(GL_VERSION) << "\n";
     glfwSetWindowTitle(window, title.str().c_str());
 
-    float triangle[6] = {
+    float vertices[8] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f
     };
-    unsigned int triangleBuffer;
-    glGenBuffers(1, &triangleBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, triangleBuffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), triangle, GL_STATIC_DRAW); // NOTE: 'size' could be sizeof the buffer like this, sizeof(buffer)
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // NOTE: 'size' could be sizeof the buffer like this, sizeof(buffer)
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    unsigned int indices[6] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgram shader{ RESOURCE_PATH "vert.glsl", RESOURCE_PATH "frag.glsl" };
     shader.bind();
@@ -58,7 +68,7 @@ int main()
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
 
